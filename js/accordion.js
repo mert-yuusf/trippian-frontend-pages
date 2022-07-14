6c83cbbe79a94d9099feffdf795179460bcdbfec
Mert izgahi <mert.izgahi@arneca.com>
2022-07-14 18:34:23 +0300

    update

--- js/accordion.js
+++ js/accordion.js
@@ -10,6 +10,7 @@ const setClassOfCategory = (question)=>{
 
 const displayQuestions = (questions)=>{
     const accordionFAQ = document.querySelector("#accordionFAQ");
+    accordionFAQ.innerHTML = "";
     accordionFAQ.innerHTML = questions.map(question => {
         return `<div class="accordion-item">
     <h2 class="accordion-header" id="heading-${question.id}">
@@ -27,31 +28,20 @@ const displayQuestions = (questions)=>{
     }).join(" ");
 }
 
-const appendAllQuestions = () => {
-    displayQuestions(FaqQuestions);
-}
-
 
 
 const searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    const query = document.querySelector("#query");
-    const questions = FaqQuestions.filter(question=>{
-        return question.answer.toLowerCase().includes(query.value.toLowerCase())
-            || question.category.toLowerCase().includes(query.value.toLowerCase())
-            || question.question.toLowerCase().includes(query.value.toLowerCase());
-    });
-
-    const accordionFAQ = document.querySelector("#accordionFAQ");
-    accordionFAQ.innerHTML = "";
-    displayQuestions(questions);
-    query.value="";
-})
-
-document.querySelector("#view-all-btn").addEventListener("click",()=>{
-    const accordionFAQ = document.querySelector("#accordionFAQ");
-    accordionFAQ.innerHTML = "";
-    displayQuestions(FaqQuestions);
-})
-appendAllQuestions();
+displayQuestions(FaqQuestions);
+setInterval(()=>{
+    const query = document.querySelector("#query").value;
+    if(query!== "" && query.length >= 3){
+        const questions = FaqQuestions.filter(question => {
+            return question.answer.toLowerCase().includes(query.toLowerCase())
+                || question.category.toLowerCase().includes(query.toLowerCase())
+                || question.question.toLowerCase().includes(query.toLowerCase());
+        });
+        displayQuestions(questions);
+    }else{
+        displayQuestions(FaqQuestions);
+    }
+},1000);
